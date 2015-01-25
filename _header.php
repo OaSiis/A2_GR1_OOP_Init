1f56c40a4e6a82aae1f57edd2850b7397570b6b5
OaSiis <maxcharbonnelle@gmail.com>
2015-01-25 19:06:14 +0100

    END

--- _header.php
+++ _header.php
@@ -5,6 +5,7 @@ session_start();
 /** @var \Doctrine\ORM\EntityManager $em */
 $em = require __DIR__.'/bootstrap.php';
 
+/** TWIG */
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem([
     __DIR__.'/views',
